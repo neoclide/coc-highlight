998f8d89d031a5c20ae5a81bf1d03ebb58b0fe68
chemzqm <chemzqm@gmail.com>
2019-08-04 00:47:57 +0800

    improve color pattern, fix #17

--- server/matchers.ts
+++ server/matchers.ts
@@ -3,9 +3,9 @@ import webColors from 'color-name'
 import { Color as VSColor, ColorInformation, Range, TextDocument } from 'vscode-languageserver'
 
 const names = Object.keys(webColors)
-const colorHex = /((?:\#)([a-f0-9]{6}([a-f0-9]{2})?|[a-f0-9]{3}([a-f0-9]{1})?))\b/gi
-const colorFunctions = /(?:(rgb|hsl)a?\([\d]{1,3}%?,\s*[\d]{1,3}%?,\s*[\d]{1,3}%?(,\s*\d?\.?\d+)?\))/gi
-const colorHwb = /(?:(hwb)\(\d+,\s*(100|0*\d{1,2})%,\s*(100|0*\d{1,2})%(,\s*0?\.?\d+)?\))/gi
+const colorHex = /(?<=\W|^)((?:#)([a-f0-9]{6}([a-f0-9]{2})?|[a-f0-9]{3}([a-f0-9]{1})?))\b/gi
+const colorFunctions = /(?:\b(rgb|hsl)a?\([\d]{1,3}%?,\s*[\d]{1,3}%?,\s*[\d]{1,3}%?(,\s*\d?\.?\d+)?\))/gi
+const colorHwb = /(?:\b(hwb)\(\d+,\s*(100|0*\d{1,2})%,\s*(100|0*\d{1,2})%(,\s*0?\.?\d+)?\))/gi
 
 export function getNameColor(word: string): VSColor | null {
   if (names.indexOf(word) == -1) return null
@@ -14,14 +14,17 @@ export function getNameColor(word: string): VSColor | null {
 }
 
 export function findColorHex(document: TextDocument): ColorInformation[] {
+  colorHex.lastIndex = 0
   return findColors(document, colorHex)
 }
 
 export function findColorFunctions(document: TextDocument): ColorInformation[] {
+  colorFunctions.lastIndex = 0
   return findColors(document, colorFunctions)
 }
 
 export function findHwb(document: TextDocument): ColorInformation[] {
+  colorHwb.lastIndex = 0
   return findColors(document, colorHwb)
 }
 
@@ -30,6 +33,7 @@ function findColors(document: TextDocument, regex: RegExp): ColorInformation[] {
   let match = regex.exec(text)
   let result: ColorInformation[] = []
   while (match !== null) {
+    console.log(match)
     const start = match.index
     try {
       const c = new Color(match[0].toLowerCase())
